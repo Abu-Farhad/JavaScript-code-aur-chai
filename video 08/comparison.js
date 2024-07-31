f0909bc2adfb34f80e86a264adc89d54a9074e87
Abu-Farhad <129104106+Abu-Farhad@users.noreply.github.com>
2024-07-31 19:57:38 +0530

    Add files via upload

---
+++ video 08/comparison.js
@@ -0,0 +1,15 @@
+console.log(Number(null));
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+console.log(null<=0);
+
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log(undefined<=0);
+
+console.log(2=="2"); // check only value
+console.log(2==="2"); // check both value and data type
