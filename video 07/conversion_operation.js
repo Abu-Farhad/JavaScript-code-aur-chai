bf93470cd5858860d198d9d993dc7d83406749d4
Abu-Farhad <129104106+Abu-Farhad@users.noreply.github.com>
2024-07-31 19:49:12 +0530

    Add files via upload

---
+++ video 07/conversion_operation.js
@@ -0,0 +1,20 @@
+let value=3;
+let negative=-value;
+console.log(value);
+console.log(negative);
+
+
+let str1="Farhad"
+let str2="Sardar"
+
+let str3=str1+" "+str2;
+console.log(str1);
+console.log(str2);
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+2+"2");
+console.log("1"+2+2);
+
+console.log(Number(""));
