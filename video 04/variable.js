2a928582ae41eb7cc1c1156c4843ff218b0e879f
Abu-Farhad <129104106+Abu-Farhad@users.noreply.github.com>
2024-07-31 08:41:12 +0530

    Add files via upload

---
+++ video 04/variable.js
@@ -0,0 +1,11 @@
+const id=39975;
+let email=`abufarhadsardar92@gmail.com`;
+var password=`hadfar`
+city=`kolkata`
+email='farhad@gmail.com'
+// id=2   //not allowed
+
+password='abu'
+city=`Basirhat`
+
+console.log([id,email,password,city])
