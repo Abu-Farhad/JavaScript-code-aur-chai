06951923057a6aa7dbcac1d980ba2957b94d2693
Abu-Farhad <129104106+Abu-Farhad@users.noreply.github.com>
2024-07-31 09:21:57 +0530

    Add files via upload

---
+++ video 05/variable.js
@@ -0,0 +1,29 @@
+"use strict";// treat all JS code as newwe version
+
+// alert() /// we are using nodejs, not browser
+
+console.log(2+3);
+console.log("Farhad")
+
+let age=22;
+let name="Farhad"
+let isLoggedIn=false;
+
+// number=> 2 ro power 53
+// bigint
+// string=>""
+// boolean= true/false
+// symbol =>unique
+
+// object
+
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isLoggedIn);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof "Farhad")
+
+// null=> object
+// undefined=> undefined
