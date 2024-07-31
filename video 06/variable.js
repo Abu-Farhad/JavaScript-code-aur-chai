00e8aa291e1258bb0db662b230ec2d9887dce7af
Abu-Farhad <129104106+Abu-Farhad@users.noreply.github.com>
2024-07-31 11:02:42 +0530

    Add files via upload

---
+++ video 06/variable.js
@@ -0,0 +1,40 @@
+let score="34"
+
+console.log(typeof score);
+console.log(typeof (scroe));
+
+let value=Number(score);
+
+console.log(value);
+console.log(typeof (value));
+
+let score1="234s";
+
+console.log(score1);
+console.log(typeof(score1));
+
+let val=Number(score1);
+console.log(val);
+console.log(typeof val);
+
+
+// "12"=>12
+// "12a"=>NaN
+
+let isloggedIn=1;
+let booleanIsLoggedInt=Boolean(isloggedIn)
+
+console.log(isloggedIn);
+console.log(typeof isloggedIn);
+
+console.log(booleanIsLoggedInt);
+console.log(typeof booleanIsLoggedInt)
+
+
+let num=123;
+console.log(num);
+console.log(typeof num);
+
+let s=String(num);
+console.log(s);
+console.log(typeof s);
