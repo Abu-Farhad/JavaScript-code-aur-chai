d252e54cfe7dc4e4d5129c57834d502ee3706381
Abu-Farhad <129104106+Abu-Farhad@users.noreply.github.com>
2024-08-06 20:09:16 +0530

    Add files via upload

---
+++ video 12/math.js
@@ -0,0 +1,12 @@
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(3.4));
+// console.log(Math.round(3.5));
+// console.log(Math.round(3.6));
+// console.log(Math.round(4.5));
+
+// console.log(Math.sqrt(2).toFixed(3));
+
+console.log(Math.random().toFixed(1)*10);
+
+console.log(Math.floor(3.5))
---
+++ video 12/number.js
@@ -0,0 +1,16 @@
+const score=78;
+const num=new Number(23);
+console.log(score,num);
+
+console.log(num.toFixed(2));
+console.log(num.toFixed(1));
+
+let nump=456;
+console.log(nump.toExponential());
+console.log(nump.toPrecision(1));
+
+const hundreds=10000000;
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-IN'));
+
+
