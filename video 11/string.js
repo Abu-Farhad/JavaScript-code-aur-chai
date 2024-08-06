956701c2c0f9ba693c244482f88a2e1fdb201772
Abu-Farhad <129104106+Abu-Farhad@users.noreply.github.com>
2024-08-06 20:05:34 +0530

    Add files via upload

---
+++ video 11/string.js
@@ -0,0 +1,25 @@
+// const name="Farhad";
+// const repoCount=2;
+
+
+// console.log(`Hello my name is ${name} and I have ${repoCount} reposetory`)
+
+// const gameName=new String("Dream League Soccer");
+
+// for(i=0;i<gameName.length;i++){
+//     console.log(`"${gameName[i]}"`);
+// }
+// console.log(gameName.toUpperCase());
+// console.log(gameName);
+// console.log(gameName.charAt(1));
+// console.log(gameName.indexOf('d'));
+
+// const newName=new String(gameName.substring(0,5));
+// console.log(newName);
+
+let myName="   Abu Farhad Sardar   "
+console.log(myName.trim());
+
+console.log(myName.trim().replace(" ","_"))
+
+
